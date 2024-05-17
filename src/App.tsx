f20c463b7f23151d0e315bfc32a34c17d94bdcfd
henrardb <bruno.henrard@gmail.com>
2024-05-17 10:39:19 +0200

    First part of ImageSlider exercice

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import "./app.css";
 //import Accordion from "./components/Accordion/Accordion";
 //import RandomColor from "./components/Random Color/RandomColor";
-import StarRating from "./components/StarRating/StarRating";
+// import StarRating from "./components/StarRating/StarRating";
+import ImageSlider from "./components/ImageSlider/ImageSlider";
 
 function App() {
   return (
     <div className="app">
       {/*<Accordion />*/}
       {/*<RandomColor />*/}
-      <StarRating numberOfStar={10} />
+      {/* <StarRating numberOfStar={10} /> */}
+      <ImageSlider
+        siteUrl={"https://picsum.photos/v2/list?page="}
+        page={2}
+        limit={10}
+      />
     </div>
   );
 }
---
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import "./ImageSlider.css";
+
+export default function ImageSlider({ siteUrl, page, limit }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState("");
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [images, setImages] = useState([]);
+
+  async function fetchImages() {
+    // console.log("fetchImage !");
+    setIsLoading(true);
+
+    try {
+      const constructedUrl = `${siteUrl}${page}&limit=${limit}`;
+      // console.log("ConstructedUrl: ", constructedUrl);
+      const response = await fetch(constructedUrl);
+      const data = await response.json();
+
+      if (data) {
+        setImages(data);
+        setIsLoading(false);
+        // console.log("Image fetched !");
+      }
+    } catch (e) {
+      setErrorMsg(e.message);
+      setIsLoading(false);
+      // console.log(`fetchImage error: ${e.message}`);
+    }
+  }
+
+  useEffect(() => {
+    if (siteUrl !== "") {
+      fetchImages();
+    }
+  }, [siteUrl]);
+
+  return (
+    <div className="content">
+      {isLoading ? (
+        <div>Pictures are loading...</div>
+      ) : (
+        images.map((imageInfo, index) => (
+          <img src={imageInfo.download_url}></img>
+        ))
+      )}
+    </div>
+  );
+}
